4e43c4be27f384c8cefaa101f403c3f9892ed1ce
Cos Kim <junzerokim@gmail.com>
2022-11-28 17:14:31 +0900

    ES6 Destructuring

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+// let arr = [2, 3, 4];
+// let [a, b, c = 10] = [];
+
+// let obj = { name: 'Kim', age: 30 };
+// let name = obj.name;
+// let age = obj.age;
+
+// let { name: 나이 } = { name: 'Kim' }; // default parameter
+
+// let name = 'Kim';
+// let age = 30;
+
+// let obj = { name: name, age: age };
+// let obj = { name, age };
+
 let obj = { name: 'Kim', age: 30 };
 
-let { name, age } = obj;
+function 함수([name, age]) {
+  console.log(name);
+  console.log(age);
+}
 
-console.log(age);
+함수([1, 2]);
