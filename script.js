adf4830590903909433a1627978d7a58688d12f0
Cos Kim <junzerokim@gmail.com>
2022-11-29 19:08:16 +0900

    callback practice

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
-// let arr = [2, 3, 4];
-// let [a, b, c = 10] = [];
-
-// let obj = { name: 'Kim', age: 30 };
-// let name = obj.name;
-// let age = obj.age;
-
-// let { name: 나이 } = { name: 'Kim' }; // default parameter
-
-// let name = 'Kim';
-// let age = 30;
-
-// let obj = { name: name, age: age };
-// let obj = { name, age };
-
-let obj = { name: 'Kim', age: 30 };
-
-function 함수([name, age]) {
-  console.log(name);
-  console.log(age);
+function func1(a) {
+  console.log(1);
+  a();
 }
 
-함수([1, 2]);
+function func2() {
+  console.log(2);
+}
+
+func1(func2);
