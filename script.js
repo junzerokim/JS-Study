e097f5553a925f0373ec0e7bc764719d3635aa57
Cos Kim <junzerokim@gmail.com>
2022-12-01 17:52:22 +0900

    ES6 async/await

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-let promise = new Promise((resolve, reject) => {
-  $.get('https://codingapple1.github.io/hello.txt').done((result) => {
-    resolve(result);
-  });
-});
+async function add() {
+  async function calc() {
+    return Promise.reject();
+  }
+  try {
+    let result = await calc();
+    console.log('성공');
+  } catch {
+    console.log('실패');
+  }
+}
 
-promise
-  .then((result) => {
-    console.log(result);
-    let promise2 = new Promise((resolve, reject) => {
-      $.get('https://codingapple1.github.io/hello2.txt').done((result) => {
-        resolve(result);
-      });
-    });
-    return promise2;
-  })
-  .then((result) => {
-    console.log(result);
-  });
+add();
