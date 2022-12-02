2dc12dd1dc3f6665b83e3a9ed6186141aeb81605
Cos Kim <junzerokim@gmail.com>
2022-12-02 18:55:48 +0900

    ES6 for in/for of

--- script.js
+++ script.js
@@ -18,6 +18,6 @@ let data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 for (let i of data) {
   for (let j of data) {
-    console.log(`${i + 1} x ${j} = ${(i + 1) * j}`);
+    console.log(`${i} x ${j} = ${i * j}`);
   }
 }
