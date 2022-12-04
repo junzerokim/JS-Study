1ab51298699aa9fa9ad0343bfb16111579777b96
Cos Kim <junzerokim@gmail.com>
2022-12-04 23:38:53 +0900

    JS Shadow

--- script.js
+++ script.js
@@ -1,14 +1,2 @@
-class hello extends HTMLElement {
-  connectedCallback() {
-    let name = this.getAttribute('name');
-    this.innerHTML = `<h2>${name}  안녕하세요</h2>`;
-  }
-  static get observedAttributes() {
-    return ['name'];
-  }
-  attributesChangedCallback() {
-    console.log(this.getAttribute('name'));
-  }
-}
-
-customElements.define('custom-input', hello);
+document.querySelector('#mordor').attachShadow({ mode: 'open' });
+document.querySelector('#mordor').shadowRoot.innerHTML = '나는 볼 수가 없음';
