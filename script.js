216308fed95da563a1da5e51f94341946b28e8ee
Cos Kim <junzerokim@gmail.com>
2022-12-05 13:32:53 +0900

    ES6 Optional Chaining

---
+++ script.js
@@ -0,0 +1,11 @@
+let user = {
+  name: 'Kim',
+  // age: { value: 30 },
+};
+
+console.log(user.age?.value);
+document.querySelector('#a')?.innerHTML;
+
+let person;
+
+console.log(person ?? '로딩중');
